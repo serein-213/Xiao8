a078bc797ec07f2200b6ddf079d7f7937606071f
wehos <wenhongz@msu.edu>
2025-07-02 09:33:47 -0700

    Remove api.py to avoid accidents. Developers should now manually copy api_template.py to api.py.

--- config/api_template.py
+++ config/api_template.py
@@ -1,16 +1,26 @@
+"""
+开发者请将api_template.py重命名为api.py后，再自行修改配置。CORE_API_KEY为必要修改项，其他可选。
 
+AUDIO_API_KEY和OPENROUTER_API_KEY的如果留空，则会默认使用CORE_API_KEY。
+如果core_config.txt中的coreApiKey被修改，则会在启动时自动覆盖CORE_API_KEY。
+"""
 # Constant for servers
-OPENAI_API_KEY = ''
-GOOGLE_API_KEY= ''
-QWEN_OMNI_URL = ''
-OPENROUTER_API_KEY = ''
-OPENROUTER_URL = ""
-VOYAGE_API_KEY = ""
+OPENROUTER_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1"#"https://openrouter.ai/api/v1"
+CORE_URL = "wss://dashscope.aliyuncs.com/api-ws/v1/realtime"#wss://api.openai.com/v1/realtime
+CORE_MODEL = "qwen-omni-turbo-realtime"#gpt-4o-realtime-preview
+MAIN_SERVER_PORT = 48911
+MEMORY_SERVER_PORT = 48912
+MONITOR_SERVER_PORT = 48913
+COMMENTER_SERVER_PORT = 48914
+CORE_API_KEY = ''
+AUDIO_API_KEY = OPENROUTER_API_KEY = ''
+VOICE_ID = 'longxiaochun_v2'
+USE_TTS = False
 
 # Variable for models
 ROUTER_MODEL = 'openai/gpt-4.1'
-SUMMARY_MODEL = 'openai/gpt-4.1-mini'
-SETTING_PROPOSER_MODEL = 'openai/gpt-4.1'
-SETTING_VERIFIER_MODEL = 'openai/o1-mini'
-SEMANTIC_MODEL = 'voyage-3-large'
-RERANKER_MODEL = 'openai/gpt-4.1'
+SUMMARY_MODEL = "qwen-plus" #'openai/gpt-4.1'
+SETTING_PROPOSER_MODEL = "qwen-max"#'openai/gpt-4.1'
+SETTING_VERIFIER_MODEL = "qwen-max"#'openai/o4-mini'
+SEMANTIC_MODEL = 'text-embedding-v4'#'text-embedding-3-small'
+RERANKER_MODEL = 'qwen-plus'#'openai/gpt-4.1'
